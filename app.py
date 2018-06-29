4520f19214adfa626d0e1170503f478903756bde
bigmore <bigmorning@live.com>
2018-06-29 14:09:00 +0700

    mo

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -10,16 +15,10 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FileMessage
 )
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from wit import Wit
 from collections import namedtuple
 import deepcut
 import os
-import requests
 import psycopg2
 
 
