ff9219c3466e516b0e22ffcda1cfd964016dd7e0
bigmore <bigmorning@live.com>
2021-07-20 13:42:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,33 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FileMessage
-)
-
-import psycopg2
-
+import requests
 
 import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-AccessToken = os.getenv('AccessToken')
-ChannelSecret = os.getenv('ChannelSecret')
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(AccessToken)
-handler = WebhookHandler(ChannelSecret)
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
+@app.route("/calen", methods=['POST', 'GET'])
+def calen():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-        print(body)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
+    state = request.headers['X-Goog-Resource-State']
+    uri = request.headers['X-Goog-Resource-URI']
+    id = request.headers['X-Goog-Channel-ID']
+    token = request.headers['X-Goog-Channel-Token']
+    return '200'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='สวัสดี'))
+url = 'https://notify-api.line.me/api/notify'
+token = 'keys'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+SECr}
+msg = 'Data TMD Ready'
+r = requests.post(url, headers=headers, data = {'message':msg})
+print(r.text)
+
+
+@app.route('/googleff9deb20e4a46255.html')
+def upload_file():
+    return 'google-site-verification: googleff9deb20e4a46255.html'
 
 
 if __name__ == "__main__":
